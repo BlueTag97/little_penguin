f4c4b57a70af08c5c235c97a01caaa827347004c
BlueTag97 <ebimhlaba@gmail.com>
2017-09-25 18:56:57 +0200

    assignment 4 added

---
+++ assignment04/main.c
@@ -0,0 +1,23 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/time.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("emmmhla");
+MODULE_DESCRIPTION("A simple hello world program.");
+MODULE_VERSION("17.08.21");
+
+static int __init hello_init(void)
+{
+	printk(KERN_INFO "Hello World!\n");
+	return (0);
+}
+
+static void __exit hello_cleanup(void)
+{
+	printk(KERN_INFO "Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
