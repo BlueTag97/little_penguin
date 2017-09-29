71e66a06f18fa53f8169231af9ea29b0c30811bd
BlueTag97 <ebimhlaba@gmail.com>
2017-09-29 03:42:51 +0200

    ex04

--- assignment04/main.c
+++ assignment04/main.c
@@ -1,22 +1,52 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/time.h>
+#include <linux/usb.h>
+#include <linux/hid.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("emmmhla");
 MODULE_DESCRIPTION("A simple hello world program.");
 MODULE_VERSION("17.08.21");
 
+static const struct usb_device_id device_table[] = {
+	{USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
+		USB_INTERFACE_SUBCLASS_BOOT,
+		USB_INTERFACE_PROTOCOL_KEYBOARD)},
+	{},
+};
+
+MODULE_DEVICE_TABLE(usb, device_table);
+
+static int probe_func(struct usb_interface *intf,
+	const struct usb_device_id *id)
+{
+	printk(KERN_INFO "Probe function.\n");
+	return 0;
+}
+
+static void disconnect_func(struct usb_interface *intf)
+{
+	printk(KERN_INFO "Disconnect function.\n");
+}
+
+static struct usb_driver device_driver = {
+	.name = "emmmhla_device_driver",
+	.id_table = device_table,
+	.probe = probe_func,
+	.disconnect = disconnect_func,
+};
+
 static int __init hello_init(void)
 {
 	printk(KERN_INFO "Hello World!\n");
-	return (0);
+	return usb_register(&device_driver);
 }
 
 static void __exit hello_cleanup(void)
 {
 	printk(KERN_INFO "Cleaning up module.\n");
+	usb_deregister(&device_driver);
 }
 
 module_init(hello_init);
